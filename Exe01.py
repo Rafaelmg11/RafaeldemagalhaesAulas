eb85ca1e8a0fe7521e2ffc1aaaa8e58c6ce9565b
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-05 17:32:15 -0300

    Informações sobre gosto. Animes e Jogos

--- Exe01.py
+++ Exe01.py
@@ -3,4 +3,11 @@ Idade: 16 anos
 Signo: Escorpião
 Hobby: Jogar volei
 Tambem gosto de jogar no xbox porem é raro ter tempo.
-Eu amo a aula de sumba mais amo mesmo a de programação de aplicativos
+Eu amo a aula de sumba mais amo mesmo a de programação de aplicativos.
+Elden Ring
+Shinobi Stracker 
+Assasins cread
+Dr. Stone 
+Shingeki no kyojin
+Maji
+Etc...
