13416fa86a443b943a8720a123c02d5a08ee6049
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-06 21:05:30 -0300

    Update Exe01.py

--- Exe01.py
+++ Exe01.py
@@ -4,10 +4,3 @@ Signo: Escorpião
 Hobby: Jogar volei
 Tambem gosto de jogar no xbox porem é raro ter tempo.
 Eu amo a aula de sumba mais amo mesmo a de programação de aplicativos.
-Elden Ring
-Shinobi Stracker 
-Assasins cread
-Dr. Stone 
-Shingeki no kyojin
-Maji
-Etc...
