887e938bea200dd35fd409e1aa6e9fcac4d59e6d
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-06 21:58:56 -0300

    Primeira aula de PYTHON

--- Exe02.py
+++ Exe02.py
@@ -1,1 +1,4 @@
 #exercicio02
+a = 2 # Variavel a recebe o valor 2;
+b = 3 # Variavel b recebe o valor 3;
+print (a+b) # print é uma função que exibe a soma de a + b;
--- Exe03.py
+++ Exe03.py
@@ -1,1 +1,6 @@
-#exercicio03
+#Faça um programa que receba um slario de 1500 reais e tenha um aumento de 5% do salario e ao final exiba o salario com o aumento
+salario = 1500
+aumento = 1500 / 100
+aumento = aumento * 5
+salario = salario + aumento
+print (salario)
