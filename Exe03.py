4be2d1d7835512ad509bba2b8f58cc740d6dcec4
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-06 22:05:40 -0300

    Update Exe03.py

--- Exe03.py
+++ Exe03.py
@@ -1,6 +1,12 @@
 #Faça um programa que receba um slario de 1500 reais e tenha um aumento de 5% do salario e ao final exiba o salario com o aumento
+#Meu codigo
 salario = 1500
 aumento = 1500 / 100
 aumento = aumento * 5
 salario = salario + aumento
 print (salario)
+
+#Codigo do Professor:
+salario = 1500
+aumento = 5
+print (salario + (salario * aumento/100))
