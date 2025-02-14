98c199ecfc309fa17d3ad7688fc93c511360763c
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-13 21:07:52 -0300

    UP

--- ExemploLista_Indice.py
+++ ExemploLista_Indice.py
@@ -8,6 +8,25 @@ print("As vendas de {} foram de {}".format(produtos[1],vendas[1]))
 
 #DESCOBRINDO O INDIDE DE X ITEM NA LISTA
 produtos = ['tv','celular','mouse','teclado','tablet']
+#i = produtos.index(input("Qual produto voce quer? "))
 i = produtos.index("mouse")
 print("O valor de i é "+ str(i))
 print("O produto da posição i é " + str(produtos[i]))
+
+
+#O ITEM X TEM NA LISTA?
+produtos = ['tv','celular','mouse','teclado','tablet']
+estoque = [100,150,180,270,80]
+
+produto = input("Insira o nome do produto e letra minuscula: ")
+
+if produto in produtos:
+    i = produtos.index(produto)
+    quant_estoque = estoque[i]
+    print ("Temos {} unidades de {} no estoque".format(quant_estoque,produto))
+else:
+    print("{} não existe no estoque".format(produto))
+
+
+  
+     
