c7ac5f2e8c4f6bc91b737a70c3dd96e30d52f12c
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-13 21:59:11 -0300

    up

--- JuntarLista.py
+++ JuntarLista.py
@@ -5,7 +5,14 @@ print(produtos)
 
 print("")
 
+print("Usando +:")
 produtos = ['apple','tv','mc','iphone x','IPad','apple watch','mac book','airpods']
 novos_produtos = ['chromecast','windows phone']
 produtos_compilado = produtos + novos_produtos
 print(produtos_compilado)
+
+print("")
+
+print("Usando Append:")
+produtos.append(novos_produtos)
+print(produtos)
