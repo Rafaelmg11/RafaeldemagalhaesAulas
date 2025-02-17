67a02170753978af1a28546632d16e6f6e99545d
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-17 20:58:54 -0300

    up

--- Exe_FOR_BREAK.py
+++ Exe_FOR_BREAK.py
@@ -10,3 +10,22 @@ for pessoa in pessoas_presentes:
         break
     else:
         print('Só um print para monstrar que ja passou por essa pessoa: '+str(pessoa))
+
+
+
+
+
+pessoas_presentes = ['John Snow','Jessie Pinkman','Aria Stark','Tyrion Lannister']
+
+#QUERO SABER SE UMA PESSOA ESPECIFICA ESTÁ PRESENTE
+
+chamada = 'Aria Stark'
+
+for pessoa in pessoas_presentes:
+    if pessoa == chamada:
+        print("{} está presente".format(chamada))
+        break
+    else:
+        print('Só um print para monstrar que ja passou por essa pessoa: '+str(pessoa))
+        continue
+        print("Só para monstrar que não vai ser printado")
