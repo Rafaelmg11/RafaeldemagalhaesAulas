eb3e7169ddedcc48c210c2998467cce3b6aaca86
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-18 19:36:30 -0300

    up

---
+++ Exe01_WHILE.py
@@ -0,0 +1,9 @@
+venda = input("Registre um produto. Para cancelar o registro de um novo produto, basta apertar enter sem digitar nada: ")
+
+vendas = []
+
+#CRIE AQUI O PROGRAMA
+while venda != '':
+    vendas.append(venda)
+    venda = input("Registre um produto. Para cancelar o registro de um novo produto,basta apertar enter sem digitar nada: ")
+print("Registro Finalizado. As vendas cadastradas foram {}".format(vendas))
