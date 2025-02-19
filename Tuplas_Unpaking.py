906b49cdef16648e21f0f1b9fa292a3e9d8af522
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-19 19:18:35 -0300

    UP

---
+++ Tuplas_Unpaking.py
@@ -0,0 +1,9 @@
+vendas = ('Rafael','06/01/2015','11/11/2008',800,'Estagiario') #TUPLA
+
+nome,data_contratacao,data_nascimento,salario,cargo = vendas #UNPACKING
+
+print(nome)
+print(data_contratacao)
+print(data_nascimento)
+print(salario)
+print(cargo)
