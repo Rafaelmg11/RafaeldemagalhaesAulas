1bd0521d17f2c621fda977590e695fd747ea6cb2
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-21 21:19:20 -0300

    up

--- Exe01_LeituraTXT.py
+++ Exe01_LeituraTXT.py
@@ -1,3 +1,3 @@
-arquivo = open ("C:/Users/rafael_a_magalhaes/Documents/PASTA TXT/GOT.txt",'r')
+arquivo = open ("C:/Users/rafael_a_magalhaes/Documents/PASTA TXT/Alunos.txt",'r')
 linhas = arquivo.readlines()
 print(linhas)
---
+++ Exe03_Funcao.py
@@ -0,0 +1,4 @@
+def print_ola(nome = "estranho"):
+    print("Olá," + nome)
+print_ola("Rafael de Almeida")
+print_ola()
