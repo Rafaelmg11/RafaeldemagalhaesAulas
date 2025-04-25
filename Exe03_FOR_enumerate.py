a88900fd0683cf102b1b7e0153ed505a5fb654b7
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-04-24 21:23:23 -0300

    Botão Pesquisar

--- Exe03_FOR_enumerate.py
+++ Exe03_FOR_enumerate.py
@@ -2,3 +2,6 @@ funcionarios = ['Maria','José','Antônio','João','Predro','Ana']
 
 for i, funcionario in enumerate(funcionarios):
     print('{} é o funcionario {}'.format(i,funcionario))
+
+for i in (funcionario):
+    print('{}'.format(i))
